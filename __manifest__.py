c0f72836f2bf9032ade1ee6ab6151a1328167d29
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-06-18 15:51:53 +0300

    [ADD] New Module openacademy

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "openacademy",
+    'author': "Ahmed Abdel baky",
+    'description': """
+                open academy modeule to manage training:
+                -manage course
+                -mange attendence
+                -mange skill    
+    """,
+    'version': '1.0',
+
+
+}
