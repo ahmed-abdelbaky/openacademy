bf4a8d0159dd35d58f947c960825bf49ca7051f9
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-06-22 22:54:23 +0300

    [ADD] two relation one one2many with session module and many2one with res.user module

--- models/academy_course.py
+++ models/academy_course.py
@@ -6,3 +6,5 @@ class academyCourse(models.Model):
     _description = 'openacademy Course '
     name = fields.Char(string='title', required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', string='Resonsible', ondelete="set null", intex=True)
+    session_ids = fields.One2many('academy.session', 'course_id', string='Session')
