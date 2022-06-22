38029496c2ad544737aad869f44ced39ce10b2c2
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-06-23 01:32:24 +0300

    [ADD] new inherit module crm and add new link to partner

--- __manifest__.py
+++ __manifest__.py
@@ -11,9 +11,14 @@
     'demo': [
         'demo.xml',
     ],
+    'depends':
+    [
+        'crm',
+    ],
     'data': [
         'views/course.xml',
         'Views/session.xml',
+        'Views/partner.xml',
     ],
 
 
