8ef7b5567ebfcc2d341db46a84d626c45a243894
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-06-22 22:56:08 +0300

    [ADD] three relation with[res.partner ,academy.course]module

--- models/academy_session.py
+++ models/academy_session.py
@@ -9,5 +9,6 @@ class academySession(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in day")
     number_of_seat = fields.Integer(string="number of seats")
-
-
+    instructor_id = fields.Many2one('res.partner', string='instructor', index=True, ondelete='cascade')
+    course_id = fields.Many2one('academy.course', string='Courses', required=True, ondelete='cascade')
+    attendence_ids = fields.Many2many('res.partner', string='set of attendees')
