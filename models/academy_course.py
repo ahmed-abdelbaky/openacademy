0533ae286d7eeb15c38001b721c9731469c97cad
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-04 22:16:11 +0300

    [ADD] add sql onstraints

--- models/academy_course.py
+++ models/academy_course.py
@@ -8,3 +8,8 @@ class academyCourse(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', string='Resonsible', ondelete="set null", intex=True)
     session_ids = fields.One2many('academy.session', 'course_id', string='Session')
+    _sql_constraints = [
+        ("Check Name", "CHECK(name != description)", "name of course must be different about description"
+         ),
+        ("UNIQUE NAME", "UNIQUE(name)", "Course Name already Exist")
+    ]
