5668ba99f665ba3cccd6448a672a1b9f419a3184
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-11 20:33:33 +0300

    [ADD]Add duplicate buttom

--- models/academy_course.py
+++ models/academy_course.py
@@ -6,7 +6,7 @@ class academyCourse(models.Model):
     _description = 'openacademy Course '
     name = fields.Char(string='title', required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one('res.users', string='Resonsible', ondelete="set null", intex=True)
+    responsible_id = fields.Many2one('res.users', string='Resonsible', ondelete="set null", index=True)
     session_ids = fields.One2many('academy.session', 'course_id', string='Session')
     _sql_constraints = [
         ("Check Name", "CHECK(name != description)", "name of course must be different about description"
